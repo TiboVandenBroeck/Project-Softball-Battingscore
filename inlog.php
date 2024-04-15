4a456f164f46206ad2e19a92479c8010ad397a26
TibovdbVives <tibo.vandenbroeck@student.vives.be>
2024-04-15 16:14:16 +0200

    First Commit

---
+++ inlog.php
@@ -0,0 +1,15 @@
+<!-- test test -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Softball-battingscore</title>
+</head>
+<body>
+    <h1>Inlog pagina</h1>
+
+    <p>log in:</p>
+    
+</body>
+</html>
